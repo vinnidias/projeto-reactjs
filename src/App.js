15aaf3a59135ae26e4205c3021ef293d085c2215
vinnidias <vinicius.3991@gmail.com>
2021-05-04 20:52:28 -0300

    exemplo de componente de classe com react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Vinicius"
+    };
+  }
+
+  handleHClick = () => {
+    const name = this.state.name
+    if(name === "Vinicius"){
+      this.setState({ name: "Dias" })
+    }
+    if(name === "Dias"){
+      this.setState({ name: "Santos" })
+    }
+    if(name === "Santos"){
+      this.setState({ name: "Vinicius" })
+    }
+  }
+
+  render() {
+
+    const { name } = this.state
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 onClick={this.handleHClick}>
+            {name}
+          </h1>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
